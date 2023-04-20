070998a6558298f342d6d10c8c0c39484d7e8490
David <david.out@migrato.nl>
2023-04-20 14:14:35 +0200

    start the program right.

--- src/Main.java
+++ src/Main.java
@@ -12,51 +12,48 @@ import java.util.Scanner;
 public class Main {
 
         public static void main(String[] args) throws MessagingException {
-            Arbeidsdeskundige arbeidsdeskundige = new Arbeidsdeskundige("davidout94@gmail.com");
-            arbeidsdeskundige.setVerstuurNaarArbeidsdeskundige(true);
-            arbeidsdeskundige.verstuurEmail();
 
-//        KenmerkenLijst kenmerkenLijst = new KenmerkenLijst();
-//        kenmerkenLijst.setUpDefaultKenmerkenLijst();
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Wat is het e-mail adres van het personeelslid?");
-//        String email = scanner.nextLine();
-//
-//        System.out.println("Wat is het huidige inkomen van het personeelslid?");
-//        double oudInkomen = scanner.nextDouble();
-//
-//        Personeelslid personeelslid = new Personeelslid(email, oudInkomen);
-//
-//        for (int i = 0; i < kenmerkenLijst.getKenmerkList().size(); i++) {
-//            System.out.println((i + 1) + " " + kenmerkenLijst.getKenmerkList().get(i).getNaam());
-//        }
-//
-//        System.out.println();
-//
-//
-//        int kenmerkNummer = 10;
-//        while (kenmerkNummer != 0) {
-//            System.out.println("Wilt u een kenmerk selecteren van het personeelslid?");
-//            System.out.println("Selecteer 0 als u geen kenmerk meer wilt toevoegen.");
-//            kenmerkNummer = scanner.nextInt();
-//
-//            if(kenmerkNummer <= 0) continue;
-//
-//            Kenmerk kenmerk = KenmerkenLijst.getKenmerkList().get(kenmerkNummer -1);
-//            if(kenmerk == null) {
-//                System.out.println("Kenmerk niet gevonden type hem alstublieft nog een keer in.");
-//                continue;
-//            }
-//
-//            personeelslid.addKenmerk(kenmerk);
-//        }
-//
-//        InkomensBerekener gegevensReceiver = new InkomensBerekener(personeelslid, null);
-//        double nieuwInkomen = gegevensReceiver.berekenInkomen();
-//
-//        System.out.println("Het nieuwe inkomen is: " + personeelslid.getNieuwInkomen().getFormatedInkomen());
+        KenmerkenLijst kenmerkenLijst = new KenmerkenLijst();
+        kenmerkenLijst.setUpDefaultKenmerkenLijst();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wat is het e-mail adres van het personeelslid?");
+        String email = scanner.nextLine();
+
+        System.out.println("Wat is het huidige inkomen van het personeelslid?");
+        double oudInkomen = scanner.nextDouble();
+
+        Personeelslid personeelslid = new Personeelslid(email, oudInkomen);
+
+        for (int i = 0; i < kenmerkenLijst.getKenmerkList().size(); i++) {
+            System.out.println((i + 1) + " " + kenmerkenLijst.getKenmerkList().get(i).getNaam());
+        }
+
+        System.out.println();
+
+
+        int kenmerkNummer = 10;
+        while (kenmerkNummer != 0) {
+            System.out.println("Wilt u een kenmerk selecteren van het personeelslid?");
+            System.out.println("Selecteer 0 als u geen kenmerk meer wilt toevoegen.");
+            kenmerkNummer = scanner.nextInt();
+
+            if(kenmerkNummer <= 0) continue;
+
+            Kenmerk kenmerk = KenmerkenLijst.getKenmerkList().get(kenmerkNummer -1);
+            if(kenmerk == null) {
+                System.out.println("Kenmerk niet gevonden type hem alstublieft nog een keer in.");
+                continue;
+            }
+
+            personeelslid.addKenmerk(kenmerk);
+        }
+
+        InkomensBerekener gegevensReceiver = new InkomensBerekener(personeelslid, null);
+        double nieuwInkomen = gegevensReceiver.berekenInkomen();
+
+        System.out.println("Het nieuwe inkomen is: " + personeelslid.getNieuwInkomen().getFormatedInkomen());
 
 
     }
